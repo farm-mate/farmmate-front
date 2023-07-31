9d294661a363cd7766c47992f753ce7ac5fc8328
Jeongbin1 <petitjb05@gamil.com>
2023-07-31 19:42:44 +0900

    fix/#30 plant post request restore

--- app/src/main/java/com/example/farmmate1/PlantAdapter.kt
+++ app/src/main/java/com/example/farmmate1/PlantAdapter.kt
@@ -23,16 +23,19 @@ class PlantAdapter (val context: Context, val PlantList: ArrayList<Plant>) : Bas
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View = LayoutInflater.from(context).inflate(R.layout.list_item_plant,null)
         val profile = view.findViewById<ImageView>(R.id.list_item_profile)
-        val name = view.findViewById<TextView>(R.id.list_item_name)
-        val startdate = view.findViewById<TextView>(R.id.list_item_startdate)
+        val plantType = view.findViewById<TextView>(R.id.list_item_name)
+        //val nickname
+        val firstPlantingDate = view.findViewById<TextView>(R.id.list_item_startdate)
         val favorite = view.findViewById<ImageView>(R.id.plant_info_back_ib)
 
         val plant = PlantList[position]
 
-        profile.setImageResource(plant.profile)
-        name.text = plant.name
-        startdate.text = plant.startdate
-        favorite.setImageResource(plant.favorite)
+        //profile.setImageResource(plant.)
+        plantType.text = plant.plantType
+        //nickname.text = plant.
+        firstPlantingDate.text = plant.firstPlantingDate
+        //북마크
+        // favorite.setImageResource(plant.favorite)
 
         return view
     }
