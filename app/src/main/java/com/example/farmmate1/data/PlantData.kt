6eddee329294126a9e9544f0e51541531e3b44ef
Jeongbin1 <petitjb05@gamil.com>
2023-07-31 17:52:29 +0900

    feat/#26 plant post request code

---
+++ app/src/main/java/com/example/farmmate1/data/PlantData.kt
@@ -0,0 +1,10 @@
+package com.example.farmmate1.data
+
+import java.util.*
+
+data class PlantData(
+    val plantType: String,
+    val plantLocation: String,
+    val memo: String,
+    val firstPlantingDate: String
+)
